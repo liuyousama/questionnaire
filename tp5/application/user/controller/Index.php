b09519c601b51a6c0510aeebe1002632095a000f
Yinqingzheng <634308664@qq.com>
2018-12-10 18:00:19 +0800

    [feat]:后台新增用户提交问卷功能

--- tp5/application/user/controller/Index.php
+++ tp5/application/user/controller/Index.php
@@ -2,13 +2,35 @@
 namespace app\user\controller;
 
 use think\Controller;
+use think\Request;
 
 class Index extends Controller
 {
     //用户首页，展示所有的问卷
     public function index(){
-        $form = model('Form')->with('FormStructure')->all();
+        $form = model('Form')->all();
         $this->assign('form',$form);
         return view();
+    }
+
+    //问卷详情，用户可以在首页点击进入并作答
+    public function detail($id,Request $request){
+        if ($request->isPost()){
+            //从前台接受生成数据
+            $data = ['time'    => time(),
+                     'form_id' => $id,
+                     'ip'      => $request->host(),
+                     'answer'  => input('post.answer')];
+            //将数据传入模拟器验证，并接受返回结果
+            $result = model('User')->answer($data);
+            //对结果进行判断，并返回对应的数据给前端
+            if ($result==1){
+                return json(['code'=>1,'msg'=>'问卷提交成功！','url'=>'index']);
+            }else{
+                return json(['code'=>0,'msg'=>$result,'url'=>'index']);
+            }
+        }
+        $form = model('Form')->where('Id',$id)->with('FormStructure')->find();
+        $this->assign('form',$form);
     }
 }
--- tp5/application/user/model/User.php
+++ tp5/application/user/model/User.php
@@ -2,9 +2,33 @@
 
 namespace app\user\model;
 
+use think\facade\Cookie;
 use think\Model;
 
 class User extends Model
 {
-    //
+    //表名
+    protected $table='user';
+
+    //用户提交问卷的答案
+    public function answer($data){
+        //查询所有提交该问卷的用户ip
+        $users = $this->where('form_id',$data['form_id'])->column('ip');
+        //如果ip地址相同，说明该设备已经提交过问卷
+        if (in_array($data['ip'],$users)){
+            return '同一问卷不可重复提交!!!';
+        }
+        //检查cookie，form_status为0代表该设备已经提交过问卷
+        if (Cookie::has('form_status')&&Cookie::get('form_status')==0){
+            return '同一问卷不可重复提价!!!';
+        }
+        //存储数据
+        if($this->save($data)){
+            //将用户已经提交过问卷的状态永久存入cookie
+            Cookie::forever('form_id',0);
+            return 1;
+        }else{
+            return '未知错误，问卷提交失败';
+        }
+    }
 }
