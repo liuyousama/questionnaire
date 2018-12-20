701b516a9ac5c04aa6157589bf994f2f06d3cffa
Yinqingzheng <634308664@qq.com>
2018-12-20 15:06:26 +0800

    [fix]:修复后台前端的一些小问题

--- tp5/application/user/controller/Index.php
+++ tp5/application/user/controller/Index.php
@@ -19,8 +19,9 @@ class Index extends Controller
             //从前台接受生成数据
             $data = ['time'    => time(),
                      'form_id' => $id,
-                     'ip'      => $request->host(),
-                     'answer'  => input('post.answer')];
+                     'ip'      => $request->ip(),
+                     'answer'  => input('post.answer'),
+                     'form_id' => $id];
             //将数据传入模拟器验证，并接受返回结果
             $result = model('User')->answer($data);
             //对结果进行判断，并返回对应的数据给前端
